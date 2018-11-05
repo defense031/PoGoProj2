fb3234a60ecbafe7a49c0fb9f49811e799113414
Austin Dennis Semmel <austinsemmel@Austins-MacBook-Pro.local>
2018-11-05 18:11:20 -0500

    gitHub

---
+++ server.R
@@ -0,0 +1,165 @@
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tidyverse)
+library(RCurl)
+library(png)
+library(knitr)
+library(ggthemes)
+
+
+#Read in advantages chart
+url1<-"https://raw.githubusercontent.com/defense031/ST590_Proj2_PoGo/master/PoGoAdvantageChart.csv"
+advantages<-read.csv(quote="",text=getURL(url1),header=TRUE)
+row.names(advantages)<-advantages[,1]
+colnames(advantages)[20]<-"noType2"
+
+#Read in Individual Pokemon Data
+url2<-"https://raw.githubusercontent.com/defense031/ST590_Proj2_PoGo/master/PoGoIndividualData.csv"
+pogo<-read.csv(text=getURL(url2),header=TRUE)
+newDPS<-rep(0,length(pogo$Pokemon))
+fastAdv<-rep(0,length(pogo$Pokemon))
+chargeAdv<-rep(0,length(pogo$Pokemon))
+totAdv<-rep(0,length(pogo$Pokemon))
+pogo<-cbind(pogo,newDPS,fastAdv,chargeAdv,totAdv)
+newData<-pogo
+
+#Read in damage mechanics external URL
+url3<-a("Damage Mechanics", href="https://pokemongo.gamepress.gg/damage-mechanics")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+  
+  #Create title page
+  output$titleText<-renderUI({
+    text<-paste0("Best Pokemon Go Raid Counters against ",str_to_title(input$bossName))
+    h3(text)
+  })
+  
+  #Create function that outputs bossName
+  output$name<-renderUI({
+    text2<-paste0(input$bossName)
+    h4(text2)
+  })
+
+  #Create link for info page
+  output$link <- renderUI({
+    tagList("URL link:", url3)
+  })
+  
+
+getData<- reactive({
+  
+  #If not sorting by generation
+  if(input$includeGens==FALSE){
+    #filter by legendary yes/no
+      if(input$leg==FALSE){
+        filterData<-filter(newData,Legendary==FALSE)
+      }else{filterData<-pogo}
+    #If sorting by generation
+  }else{
+    #sorting by generation with legendary selected FALSE
+    if(input$leg==FALSE){
+      filterData<-filter(newData[newData$Generation %in% input$gens,])
+      filterData<-filter(filterData,Legendary==FALSE)
+                         
+      #sorting by generation with legendary selected TRUE
+    }else{filterData<-filter(newData[newData$Generation %in% input$gens,])
+    }
+  }
+
+  #Code to find type1 of raid boss
+  bossType1<-as.character(pogo[which(pogo$Pokemon==input$bossName)[1],2])
+  #Code to find type2 of raid boss
+  bossType2<-ifelse(as.character(pogo[which(pogo$Pokemon==input$bossName)[1],3])=="","noType2",as.character(pogo[which(pogo$Pokemon==input$bossName)[1],3]))
+  #boss advantages chart for raid boss
+  bossAdvantages<-advantages[c(bossType1,bossType2)]
+  
+  #Initialize vector to catch fastType, fastAdv, chargeType
+  
+  for(i in 1:length(filterData$Pokemon)){
+    #Find fast type advantage for each Pokemon
+    filterData$fastAdv[i]<-bossAdvantages[as.character(filterData$FastType[i]),bossType1]*bossAdvantages[as.character(filterData$FastType[i]),bossType2]
+    #Find charge type advantage for each Pokemon
+    filterData$chargeAdv[i]<-bossAdvantages[as.character(filterData$ChargedMoveType[i]),bossType1] * bossAdvantages[as.character(filterData$ChargedMoveType[i]),bossType2]
+    #Find DPS Modifier for each Pokemon
+    filterData$totAdv[i]<-mean(c(filterData$chargeAdv[i],filterData$fastAdv[i]))
+    filterData$newDPS[i]<-filterData$DPS[i]*filterData$totAdv[i]
+  }
+  #Now find top n in adjusted DPS vs raid boss
+  top<-arrange(filterData,desc(newDPS))[1:input$numMon,]
+  nameCat<-paste0(top$Pokemon," ",top$FastMove," ",top$ChargedMove)
+  top<-cbind(top,nameCat)
+  top<-top
+  
+})
+
+
+#Create DPS plot
+  output$DPSPlot<-renderPlot({
+    top<-getData()
+    g<-ggplot(data=top)
+    g+geom_point(aes(x=top$nameCat,y=top$newDPS),color=top$TypeColor,
+                 size=10*percent_rank(top$TDO))+
+      theme_solarized()+
+      theme(axis.text.x=element_text(angle=60,hjust=1))+
+      xlab("")+ylab("Adjusted DPS")
+  })
+
+#Create DPS vs Health plot
+  output$HealthPlot<-renderPlot({
+    top<-getData()
+    h<-ggplot(data=top,aes(x=top$Stamina,y=top$newDPS),color=top$TypeColor)
+    h+geom_point()+
+      geom_text(aes(label=top$nameCat),hjust=.1,vjust=1)+
+      theme_solarized()+
+      xlab("Health")+ylab("Adjusted DPS")
+  }) 
+  
+  
+  
+  
+#Create output of observations    
+  output$DPStable <- renderTable({
+    top<-getData()
+    sumTable<-top[,c(1,2,3,4,6,8,9,10,18,12,16)]
+    sumTable[,9]<-round(sumTable[,9],digits=1)
+    colnames(sumTable)<-c("Pokemon","Type 1", "Type 2","Fast Move", "Charged Move","Stamina","Attack","Defense","Adj. DPS","Total Damage Output","Leg.")
+    sumTable
+  })
+  
+#Download Data
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Top Counters" = getData(),
+           "Full Dataset" = pogo,
+           "Type Advantages"=advantages
+           )
+  })
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file)
+    }
+  )
+
+#Download plot1
+  output$downloadPlot1<- downloadHandler(
+    filename = 'test.png',
+    content = function(file) {
+      device <- function(..., width, height) {
+        grDevices::png(..., width = width, height = height,
+                       res = 300, units = "in")
+      }
+      ggsave(file, plot = DPSPlot, device = device)
+    })
+  
+                                           
+  
+#End of app
+})
+  
+
---
+++ ui.R
@@ -0,0 +1,128 @@
+#Austin Semmel Project 2
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(knitr)
+library(shinythemes)
+library(shinydashboard)
+library(RCurl)
+library(shinydashboardPlus)
+
+url2<-"https://raw.githubusercontent.com/defense031/ST590_Proj2_PoGo/master/PoGoIndividualData.csv"
+pogo<-read.csv(text=getURL(url2),header=TRUE)
+
+shinyUI(dashboardPagePlus(skin="blue",
+  
+  # Application title
+  dashboardHeaderPlus( 
+    title=uiOutput("titleText"),
+    titleWidth=700,
+    tags$li(class = "dropdown",
+            tags$style(".main-header {max-height: 75}"),
+            tags$style(".main-header .logo {height: 75;}"),
+            tags$style(".sidebar-toggle {height: 75; padding-top: 1px !important;}"),
+            tags$style(".navbar {min-height:75 !important}")
+    )
+  ),
+  # Sidebar 
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About this App", tabName="about",icon=icon("question",lib="font-awesome")),
+      menuItem("Dashboard", tabName = "dashboard", icon = icon('line-chart'),
+                    menuSubItem('DPS for each Mon', tabName = 'subDPS1',selected=TRUE) ,
+                    menuSubItem('DPS vs Health', tabName = 'subDPS2') 
+      ),
+               
+      menuItem("Data", tabName = "data",icon = icon("th"))
+      ),
+      br(),
+      br(),
+      textInput("bossName","What Pokemon are you battling against?",value="Charizard"),
+      br(),
+     checkboxInput("leg","Do you want to include legendaries?",value=TRUE),
+      br(),
+     #Generation conditional panel
+     checkboxInput("includeGens","Do you want to separate by Generation?",value=FALSE),
+     conditionalPanel(condition="input.includeGens=='1'",
+      checkboxGroupInput("gens","Which Generations do you want to include?",
+                       choices=levels(pogo$Generation),inline=TRUE)
+      ),
+    sliderInput(inputId="numMon",
+                label="How many counters would you like to display?",
+                min = 6,
+                max = 12,
+                value = 6
+    )
+    ),
+    #Main Panel
+    dashboardBody(
+      tabItems(
+          tabItem(tabName="about",
+                h3("How This App Works and Other Cool Info"),
+                fluidRow(
+                  box(title="Motivation for App",h4(
+                  "Pokemon Go Raids are pretty popular in Durham and Raleigh these days.
+                  People gather together at random points of interest called 'gyms' and
+                  battle against a computer boss Pokemon.  Tier 5 Raids can be pretty difficult!
+                  You only get one unpaid raid per day, so you have to make them count.
+                  To do so, you need a good knowledge of how to make a party based on
+                  what Pokemon you are going up against!  This app will help show you the 
+                  best counters against a boss based on their type-adjusted Damage Per Second
+                  (DPS).  The formulas on the right show how I calculated DPS and adjusted DPS based
+                  on real data from the original games.  These are the same formulas that the
+                  developers use (minus Mewtwo who infamously received a nerf for being OP).
+                  Check out this link for more info on damage mechanics!
+                  "),
+                  uiOutput("link")
+                  ),
+                  box(title="Formulas",
+                      withMathJax(),
+                      helpText("The initial damage of a single move is calculated via this formula rounded down:
+                               $$Damage=Power*\\frac{Attack}{Defense}*Multiplier)+1$$
+                                The damage multiplier, though, changes based on the type of attack being used
+                                and the opponent's type.  For example, a pokemon using a 'Fire' based attack
+                                would be very effective against 'grass' but ineffective against 'water.'
+                                We can dynamically find the damage-type multiplier for each pokemon based on the raid boss.
+                                $$Fast Atk Mult=$$
+                                $$(FastMove*BossType1)*(FastMoveAdv*BossType2)$$
+                                $$Charged Atk Mult=$$
+                                $$(ChargedMoveAdv*BossType1)*(ChargedMoveAdv*BossType2)$$
+                                $$Total Damage Multiplier=$$
+                                $$Average(Fast Mult,Charged Mult)$$
+                                Assuming that roughly half of the damage is from the fast attack and half
+                                is from the charged attack.  Then, the Adjusted DPS is:
+                                $$AdjDPS= DPS * Total Multiplier$$
+                                You can see the values for this chart in the 'Advantages' table.
+                               ")
+                    )
+                  
+                )
+          
+          ),
+          tabItem(tabName="subDPS1",
+                  h3("Plot of Top Adjusted DPS"),
+                  fluidRow(plotOutput("DPSPlot")),
+                  fluidRow(h4("This plot displays the top Pokemon in terms of DPS against ", uiOutput("name"))),
+                  downloadButton('downloadPlot1', 'Download Plot')
+                  
+          ),
+          tabItem(tabName="subDPS2",
+                  h3("Plot of Adjusted DPS vs. Health"),
+                  fluidRow(plotOutput("HealthPlot")),
+                  fluidRow(h4("This graph shows a plot of each Pokemon's DPS vs its health.  
+                               This could be of value if you want to make sure you don't simply have
+                               Pokemon that are 'glass cannons.'")),
+                  downloadButton('downloadPlot2', 'Download Plot')
+                  
+          ),
+          tabItem(tabName="data",
+              h3("Top 'Mon"),
+              fluidRow(tableOutput("DPStable")),
+              selectInput("dataset", "Choose a dataset:",
+                          choices = c("Top Counters","Full Dataset","Type Advantages")),
+              downloadButton("downloadData","Download Data")
+          )
+    )
+    )
+)
+)
